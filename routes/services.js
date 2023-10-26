ae9d93180f8195d6c41dcdb6ad9ff25dc577aa88
Aleksei Seletckii <alexey.sv.97@gmail.com>
2023-10-26 12:47:48 -0400

    13th commit

--- routes/services.js
+++ routes/services.js
@@ -7,7 +7,7 @@ const User = require('../models/User')
 const isBusiness = require('../middleware/isBusiness')
 const isAuthenticated = require('../middleware/isAuthenticated')
 const isServiceOwner = require('../middleware/isServiceOwner')
-
+const axios = require('axios')
 
 router.get('/', (req, res, next) => {
     Service.find()
@@ -23,19 +23,72 @@ router.get('/', (req, res, next) => {
         })
 })
 
-router.post('/new', isAuthenticated, isBusiness, (req, res, next) => {
+router.post('/new', isAuthenticated, isBusiness, (req, res, next) => { // async
 
     if (!req.body.category) {
         res.status(400).json({ message: "Provide a category." });
         return;
     }
 
-    
+// const saveServiceLocation = async (serviceData) => {
+//     const { location } = serviceData
+
+//     const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+//         params: {
+//             location,
+//             key: 'AIzaSyCRTE2lYv81Hmqzw9SKKF2bRUflzjFQOTI'
+//         }
+//     })
+
+//   const { results } = response.data;
+//   if (results.length > 0) {
+//     const { lat, lng } = results[0].geometry.location;
+
+   
+//     return [lat,lng]
+//   } else return
+// }
 
     Service.create({
         ...req.body,
+        // latitude: await saveServiceLocation(req.body)[0],
+        // longitude: await saveServiceLocation(req.body)[1],
         user: req.user._id
     })
+// const saveServiceLocation = async (serviceData) => {
+//     const { location } = serviceData;
+
+//     try {
+//         const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+//             params: {
+//                 address: location,  // Use 'address' instead of 'location'
+//                 key: 'AIzaSyCRTE2lYv81Hmqzw9SKKF2bRUflzjFQOTI'
+//             }
+//         });
+
+//         const { results } = response.data;
+//         if (results.length > 0) {
+//             const { lat, lng } = results[0].geometry.location;
+//             return [lat, lng];
+//         } else {
+//             return [null, null]; // Handle cases where no results are found
+//         }
+//     } catch (error) {
+//         console.error('Error during geocoding:', error);
+//         return [null, null];
+//     }
+// };
+
+// // Use the function within an async context to await its response
+// const coordinates = await saveServiceLocation(req.body);
+// console.log("coordinates ===>", coordinates)
+
+// Service.create({
+//     ...req.body,
+//     latitude: coordinates[0],
+//     longitude: coordinates[1],
+//     user: req.user._id
+// })
         .then((createdService) => {
             User.findByIdAndUpdate(
                 req.params.serviceId,
@@ -44,8 +97,8 @@ router.post('/new', isAuthenticated, isBusiness, (req, res, next) => {
                 },
                 { new: true }
             )
-                .then((newUser) => {
-                    console.log("New USer", newUser)
+                .then((newService) => {
+                    console.log("New service", newService)
                 })
                 .catch((err) => {
                     console.log(err)
